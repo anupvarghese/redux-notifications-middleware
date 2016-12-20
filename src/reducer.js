8ca1ecee73abfd8c6e8bca3a7ba87a094c7de7fc
Anoop Varghese <anupvarghese@gmail.com>
2016-12-20 16:25:44 +1100

    modify to accept type

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,8 @@ import C from './constants';
 export default(state = [], action) => {
   switch (action.type) {
     case C.SHOW_NOTIFICATION: {
-      const { notificationPayload, id, notificationDelay } = action;
-      return [...state, { notificationPayload, notificationDelay, id }];
+      const { notificationPayload, id, notificationDelay, notificationType } = action;
+      return [...state, { notificationPayload, notificationDelay, id, notificationType }];
     }
     case C.HIDE_NOTIFICATION: {
       return [...state].filter(n => n.id !== action.id);
